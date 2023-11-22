bd965ab682addfc8bbeaf0336ed26fcc4c4087e5
juratdev <juratdev@gmail.com>
2023-11-22 19:02:21 +0500

    New category added

--- js/main.js
+++ js/main.js
@@ -1,24 +1,21 @@
-const initApp = () => {
-  const hamburgerBtn = document.getElementById('hamburger-button')
-  const mobileMenu = document.getElementById('mobile-menu')
+const hamburgerBtn = document.getElementById('hamburger-button')
+const mobileMenu = document.getElementById('mobile-menu')
 
-  const toggleMenu = () => {
-    mobileMenu.classList.toggle('hidden')
-    mobileMenu.classList.toggle('flex')
-    hamburgerBtn.classList.toggle('toggle-btn')
-  }
-
-  hamburgerBtn.addEventListener('click', toggleMenu)
-  mobileMenu.addEventListener('click', toggleMenu)
-
-  hamburgerBtn.addEventListener('click', () => {
-    document.body.classList.toggle('overflow-hidden')
-
-  })
-  mobileMenu.addEventListener('click', () => {
-    document.body.classList.toggle('overflow-hidden')
-
-  })
+const toggleMenu = () => {
+  mobileMenu.classList.toggle('hidden')
+  mobileMenu.classList.toggle('flex')
+  hamburgerBtn.classList.toggle('toggle-btn')
 }
 
-document.addEventListener('DOMContentLoaded', initApp)
+
+hamburgerBtn.addEventListener('click', toggleMenu)
+mobileMenu.addEventListener('click', toggleMenu)
+
+// Hamburger Menu overflow-hidden
+hamburgerBtn.addEventListener('click', () => {
+  document.body.classList.toggle('overflow-hidden')
+})
+mobileMenu.addEventListener('click', () => {
+  document.body.classList.toggle('overflow-hidden')
+
+})
